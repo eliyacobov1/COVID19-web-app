5d9b03c98ff898fcfb9d3e46ad48be1bcb9772cc
Eli Yacobov <eli.yacobov1@gmail.com>
2021-05-20 22:45:25 +0300

    .

--- server/temp-data.ts
+++ server/temp-data.ts
@@ -1,8 +1,6 @@
-import {Country, Ticket} from '../client/src/api';
+import {Country} from '../client/src/api';
 
 
-const data = require('./data.json');
 const country_data = require('./country_data.json');
 
-export const tempData = data as Ticket[];
 export const countryData = country_data as Country[];
--- server/utils.ts
+++ server/utils.ts
@@ -1,4 +1,4 @@
-import {tempData, countryData} from './temp-data'
+import {countryData} from './temp-data'
 
 /**
  * (Q2.c + 2.d) this function performs a search over the data-base
@@ -20,19 +20,6 @@ export const uniqueID = () => {
         '-' + chr4() +
         '-' + chr4() +
         '-' + chr4() + chr4() + chr4();
-}
-
-/**
- * find the index of the ticket with the
- * given id inside of the DataBase array
- */
-export const findTicketIndex= (id: string) => {
-    for(let i = 0; i < tempData.length; i += 1) {
-        if(tempData[i].id === id) {
-            return i;
-        }
-    }
-    return -1;
 }
 
 /**
